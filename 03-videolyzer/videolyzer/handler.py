e4b2bbcae8f88dc384c0d16e883022de3c0db5f0
Tom Coyle <guitardude0825@yahoo.com>
2020-08-25 12:20:22 -0400

    Save data from get_label_detection to DynamoDB

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,3 +1,6 @@
+"""Handle video uploads to S3 and analyze them with AWS Rekognition."""
+
+
 import json
 
 import os
@@ -6,7 +9,9 @@ import urllib
 
 import boto3
 
+
 def start_label_detection(bucket, key):
+    """Kick off label detection with Rekognition."""
     rekognition_client = boto3.client('rekognition')
     response = rekognition_client.start_label_detection(
         Video={
@@ -20,11 +25,11 @@ def start_label_detection(bucket, key):
             'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
 
-    print(response)
-
     return
 
+
 def get_video_labels(job_id):
+    """Get video labels."""
     rekognition_client = boto3.client('rekognition')
 
     response = rekognition_client.get_label_detection(JobId=job_id)
@@ -43,12 +48,44 @@ def get_video_labels(job_id):
 
     return response
 
+
+def make_item(data):
+    """Convert data types."""
+    if isinstance(data, dict):
+        return {k: make_item(v) for k, v in data.items()}
+
+    if isinstance(data, list):
+        return [make_item(v) for v in data]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    """Store video label data in DynamoDB."""
+    del data['ResponseMetadata']
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
 
 # Lambda events
 
+
 def start_processing_video(event, context):
+    """Process video after it is uploaded to S3."""
     for record in event['Records']:
         start_label_detection(
             record['s3']['bucket']['name'],
@@ -57,7 +94,9 @@ def start_processing_video(event, context):
 
     return
 
+
 def handle_label_detection(event, context):
+    """Detect video labels."""
     for record in event['Records']:
         message = json.loads(record['Sns']['Message'])
         job_id = message['JobId']
@@ -65,7 +104,6 @@ def handle_label_detection(event, context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
